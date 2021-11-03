837de1ed3321e8afad31c279026b76b83bfcc938
monish karunakaran <ckmonish2000@gmail.com>
2021-11-03 19:44:28 +0530

    varibles

--- src/modules/variables.rs
+++ src/modules/variables.rs
@@ -1,5 +1,12 @@
 pub fn vars(){
   let x = 45;
   let mut y = 60;
-  println!("var = {} mutable varible ={}",x,y);
+  
+  let x2: i32 = -20;
+  let f: f64 = 30.00;
+  let b: bool = false;
+  
+  println!("var = {} mutable varible = {}",x,y);
+
+  println!("assigned type = {},{},{}",x2,f,b);
 }
