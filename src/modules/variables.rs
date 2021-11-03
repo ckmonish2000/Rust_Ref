ced1cf2c551b9ed97599763c7f53f8f62626cf35
monish karunakaran <ckmonish2000@gmail.com>
2021-11-03 19:46:12 +0530

    minor

--- src/modules/variables.rs
+++ src/modules/variables.rs
@@ -2,6 +2,9 @@ pub fn vars(){
   let x = 45;
   let mut y = 60;
   
+  /* refer more types from 
+  (https://doc.rust-lang.org/book/ch03-02-data-types.html) */
+  
   let x2: i32 = -20;
   let f: f64 = 30.00;
   let b: bool = false;
