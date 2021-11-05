c1c633dd08f95c2e59146adc868c8902ecaab844
monish karunakaran <ckmonish2000@gmail.com>
2021-11-05 21:17:31 +0530

    struct tuple

--- src/modules/structures.rs
+++ src/modules/structures.rs
@@ -1,3 +1,4 @@
+// basic struct
 #[derive(Debug)]
 struct Color{
   r: u8,
@@ -5,8 +6,20 @@ struct Color{
   b: u8,
 }
 
+// tuple struct
+#[derive(Debug)]
+struct Colors(u8,u8,u8);
+
 pub fn structures(){
+  
+  // basic struct
   let mut bg_color = Color{r:0,g:0,b:0};
   bg_color.r = 255;
   println!("{:?}", bg_color);
+
+  // tuple struct
+  let color = Colors(255,255,255);
+  println!("{:?}", color);
+
+
 }
