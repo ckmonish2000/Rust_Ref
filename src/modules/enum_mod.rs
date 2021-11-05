161c33dea59931a22a0ec4185904f807ec179125
monish karunakaran <ckmonish2000@gmail.com>
2021-11-05 20:27:51 +0530

    minor

--- src/modules/enum_mod.rs
+++ src/modules/enum_mod.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
+
 pub fn enum_mod(){
-  
+  let player_direction: Direction = Direction::Up;
+
+  println!("{:?}", player_direction);
 }
