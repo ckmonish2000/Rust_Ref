daf03f371f6d7315580b2af28ccc46231dfc67e5
monish karunakaran <ckmonish2000@gmail.com>
2021-11-05 20:05:42 +0530

    for

--- src/modules/forloop.rs
+++ src/modules/forloop.rs
@@ -1,7 +1,15 @@
 pub fn forloop(){
-
+  // 2nd part of for must be a iterator
+  
+  let range  = 0..11;
   // 0..10 range from 0 to 9
-  for i in 0..10{
+
+  let animals: Vec<&str> = vec!["dog","cat"];
+  
+  // using iter will not transfer ownership 
+  for i in animals.iter(){
     println!("{}",i);
   }
+
+  println!("{}",animals[0]);
 }
