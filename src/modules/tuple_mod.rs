f521c1ff181d644248515a64ed0cef86174ea60b
monish karunakaran <ckmonish2000@gmail.com>
2021-11-05 20:40:46 +0530

    tuples

--- src/modules/tuple_mod.rs
+++ src/modules/tuple_mod.rs
@@ -1,3 +1,9 @@
 pub fn tuple_mod(){
-  
+  let tup1: (u32,u32,f32,&str) = (10,20,30.5,"string");
+  let tup2 = (10,20,30.5,"string",(1,4,7)); 
+  // both tup 1 and 2 are same
+
+
+  // refrencing to a particular val 
+  println!("{:?}",tup2.4.2);
 }
