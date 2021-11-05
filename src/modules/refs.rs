c1e73d148b233a5451549dd3880170032e0e7d67
monish karunakaran <ckmonish2000@gmail.com>
2021-11-05 21:08:25 +0530

    ref

--- src/modules/refs.rs
+++ src/modules/refs.rs
@@ -1,8 +1,14 @@
 pub fn refs(){
 
   let mut name ="domenic";
-  let name_r = &mut name;
+  
+  {
+    let name_r_2 = &name; // immutable reference
+    let name_r = &mut name; // mutable refrence
+    *name_r = "dom";
+  }
 
-  *name_r = "dom";
-  println!("{} ",name);
+  //  in a given line we can have multiple refrences for  given var
+  //  but we can have only one refrence to a mutable refrence
+  println!("{}",name);
 }
