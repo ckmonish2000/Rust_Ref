d20effbd7c263baee4645c64338d35623cd7ee57
monish karunakaran <ckmonish2000@gmail.com>
2021-11-05 21:28:54 +0530

    minor

--- src/modules/array.rs
+++ src/modules/array.rs
@@ -1,8 +1,9 @@
 pub fn array(){
   let numbers = [1,2,3,4,5];
   let num:[u32;2] = [1,2]; // static declaration
+  let num2 = [2;10];// default values
   
-  println!("{}",numbers[0]);
+  println!("{}",num2[0]);
   
   for i in numbers.iter() {
     println!("{}",i);
