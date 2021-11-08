dc8c05e94761f0f7ce21c862f54d2c412882ea9c
monish karunakaran <ckmonish2000@gmail.com>
2021-11-08 22:28:52 +0530

    read from file

--- src/modules/readfile.rs
+++ src/modules/readfile.rs
@@ -1,6 +1,17 @@
-use std::fs::File;
+use std::fs::*;
+use std::env;
 use std::io::prelude::*;
 
 pub fn read_file(){
   
+  let mut path = env::current_dir();
+  let mut path = String::from(path.unwrap().to_str().unwrap());
+  path.push_str("\\src\\modules\\info.txt");
+
+  let mut file = File::open(path).expect("cant open file");
+  let mut content = String::new();
+
+  file.read_to_string(&mut content).unwrap();
+
+  println!("{}", content);
 }
