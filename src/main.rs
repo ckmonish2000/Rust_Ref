9b7c77f0afd72f2a0caa5223943b7d84e57b1481
monish karunakaran <ckmonish2000@gmail.com>
2021-11-10 13:33:05 +0530

    custom traits

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,8 @@ fn main() {
     // modules::structures::structures();
     // modules::array::array();
     // modules::struct2::struct2();
-    // modules::traits::traits();
+    modules::traits::traits();
     // modules::readfile::read_file();
     // modules::evn_var::evn_var();
-    modules::write::write();
+    // modules::write::write();
 }
--- src/modules/traits.rs
+++ src/modules/traits.rs
@@ -3,12 +3,30 @@ struct Person{
   age: u8
 }
 
+// custom traits 
+trait HasVoiceBox{
+  fn speak(&self);
+  fn canspeak(&self,values:bool)->bool;
+}
+
+
 impl Person {
   fn new(name:String,age:u8) -> Person {
     Person { name :name, age :age }
   }
 }
 
+impl HasVoiceBox for Person{
+  fn speak(&self){ 
+    println!("hello");
+  }
+
+  fn canspeak(&self,value:bool) -> bool{
+    return value;
+  }
+}
+
+// predefined traits
 impl ToString for Person {
   fn to_string(&self) -> String {
     return format!("name = {} age ={}", &self.name,&self.age);
@@ -17,6 +35,10 @@ impl ToString for Person {
 
 pub fn traits(){
 let dom = Person::new(String::from("mk"), 21);
+
+// custom traits call
+println!("{}",dom.canspeak(false));
+dom.speak();
 
 println!("{}",dom.to_string());
 }
